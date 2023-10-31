7708f76da974930c53841805141f2315423b112f
Pietro907 <pietrop.puri@gmail.com>
2023-10-31 17:32:11 +0100

    add table on migration create_comics_table

--- app/Models/Comics.php
+++ app/Models/Comics.php
@@ -2,10 +2,23 @@
 
 namespace App\Models;
 
+
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Comics extends Model
 {
     use HasFactory;
+
+
+/*     protected function address(): Comics
+{
+    return Comics::make(
+        get: fn (mixed $comics, array $comic) => new Comics(
+            $comic['title'],
+            $comic['thumb'],
+        ),
+    );
+} */
+
 }
--- database/migrations/2023_10_31_144717_create_comics_table.php
+++ database/migrations/2023_10_31_144717_create_comics_table.php
@@ -6,20 +6,25 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
+
     public function up(): void
     {
         Schema::create('comics', function (Blueprint $table) {
             $table->id();
+            $table->string('title', 50);
+            $table->text('description')->nullable();
+            $table->longText('thumb')->nullable();
+            $table->string('price')->nullable();
+            $table->string('series', 50)->nullable();
+            $table->date('sale_date')->nullable();
+            $table->string('type')->nullable();
+            $table->string('artists');
+            $table->string('writers');
             $table->timestamps();
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
+
     public function down(): void
     {
         Schema::dropIfExists('comics');
